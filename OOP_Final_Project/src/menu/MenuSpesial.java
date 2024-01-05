06ca705eab24e92bffac8d88014713eba200da74
KevinAlvero <152252460+KevinAlvero@users.noreply.github.com>
2024-01-05 12:35:25 +0700

    Update MenuSpesial.java

--- OOP_Final_Project/src/menu/MenuSpesial.java
+++ OOP_Final_Project/src/menu/MenuSpesial.java
@@ -1,5 +1,24 @@
 package menu;
 
-public class MenuSpesial {
+public class MenuSpesial extends Menu {
+    private String narasi;
 
+    public MenuSpesial(int id, String nama, int harga, String narasi) {
+        super(id, nama, harga);
+        this.narasi = narasi;
+    }
+
+    public String getNarasi() {
+        return narasi;
+    }
+
+    public void setNarasi(String narasi) {
+        this.narasi = narasi;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "Narasi: " + narasi + "\n";
+    }
 }
+
