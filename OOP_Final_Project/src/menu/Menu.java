95c2344ff7cdb326e9060798162a39391ba5b4f6
KevinAlvero <152252460+KevinAlvero@users.noreply.github.com>
2024-01-05 12:33:33 +0700

    Menu.java

--- OOP_Final_Project/src/menu/Menu.java
+++ OOP_Final_Project/src/menu/Menu.java
@@ -1,5 +1,31 @@
 package menu;
 
 public class Menu {
+    private int id;
+    private String nama;
+    private int harga;
 
+    public Menu(int id, String nama, int harga) {
+        this.id = id;
+        this.nama = nama;
+        this.harga = harga;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id + "\nNama: " + nama + "\nHarga: " + harga + "\n";
+    }
 }
+
