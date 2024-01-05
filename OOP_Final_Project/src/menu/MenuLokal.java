60de15b500cd9d36f181906c03ddff9effbe729b
KevinAlvero <152252460+KevinAlvero@users.noreply.github.com>
2024-01-05 12:35:03 +0700

    MenuLokal.java

--- OOP_Final_Project/src/menu/MenuLokal.java
+++ OOP_Final_Project/src/menu/MenuLokal.java
@@ -1,5 +1,26 @@
 package menu;
 
-public class MenuLokal {
+public class MenuLokal extends Menu {
+    private String lokasi;
+    private String ciriKhas;
 
+    public MenuLokal(int id, String nama, int harga, String lokasi, String ciriKhas) {
+        super(id, nama, harga);
+        this.lokasi = lokasi;
+        this.ciriKhas = ciriKhas;
+    }
+
+    public String getLokasi() {
+        return lokasi;
+    }
+
+    public String getCiriKhas() {
+        return ciriKhas;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "Lokasi: " + lokasi + "\nCiri Khas: " + ciriKhas + "\n";
+    }
 }
+
